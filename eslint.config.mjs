c4035682155f810a06e6862cc3922687faa90492
hannah leong <hannah.leong@digital.cabinet-office.gov.uk>
2024-07-11 11:26:39 +0100

    ATO-857: Add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+    {files: ["**/*.{js,mjs,cjs,ts}"]},
+    {languageOptions: {globals: {...globals.browser, ...globals.node, ...globals.jest}}},
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    {rules: {"@typescript-eslint/no-var-requires": "off"}},
+];
