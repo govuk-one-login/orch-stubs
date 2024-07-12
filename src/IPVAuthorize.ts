116c13b1ca9992ed2fe0562643d94c3fe0d5817a
hannah leong <hannah.leong@digital.cabinet-office.gov.uk>
2024-07-12 08:59:54 +0100

    ATO-857: Change structure to only lambdas
    Mis-interpretation so re-done:
    - Removed `app.ts`
    - Added base of IPV authorize lambda

---
+++ src/IPVAuthorize.ts
@@ -0,0 +1,42 @@
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Handler,
+} from "aws-lambda";
+
+export const handler: Handler = async (
+  event: APIGatewayProxyEvent,
+): Promise<APIGatewayProxyResult> => {
+  switch (event.httpMethod) {
+    case "GET":
+      return get(event);
+    case "POST":
+      return post(event);
+    default:
+      return {
+        statusCode: 405,
+        body: JSON.stringify({
+          message: "Method not allowed",
+        }),
+      };
+  }
+};
+
+function get(event: APIGatewayProxyEvent): APIGatewayProxyResult {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `Reached the ${event.httpMethod} endpoint`,
+    }),
+  };
+}
+
+function post(event: APIGatewayProxyEvent): APIGatewayProxyResult {
+  console.log("I'm going to save the form to the database");
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `Reached the ${event.httpMethod} endpoint - this will return 302`,
+    }),
+  };
+}
