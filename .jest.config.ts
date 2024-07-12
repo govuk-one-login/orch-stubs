baaf624a291ada50c2ea37e41ed920ff2289aa5a
hannah leong <hannah.leong@digital.cabinet-office.gov.uk>
2024-07-12 16:25:46 +0100

    ATO-857: Add jest testing

---
+++ .jest.config.ts
@@ -0,0 +1,11 @@
+export default {
+    clearMocks: true,
+    collectCoverage: true,
+    collectCoverageFrom: ["**/*.test.ts"],
+    coverageDirectory: "coverage",
+    preset: "ts-jest",
+    testEnvironment: "node",
+    testPathIgnorePatterns: [
+        "/node_modules/"
+    ]
+};
---
+++ test/example.test.ts
@@ -0,0 +1,7 @@
+import {example} from "./example";
+
+describe("Example test", () => {
+    test('adds 1 + 2 to equal 3', () => {
+        expect(example(1, 2)).toBe(3);
+    })
+});
