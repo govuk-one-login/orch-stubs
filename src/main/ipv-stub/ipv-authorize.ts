e28ac82e454af7b3f9776e28407de70fad12c73a
Bill Dunn <bill.dunn@digital.cabinet-office.gov.uk>
2024-08-01 09:05:20 +0100

    ATO-861: Create GET method logic for IPV Authorize lambda
    Processing of the token:
    - Extracted from header
    - Decrypted
    - Decoded
    - Displayed on form page

--- src/main/ipv-stub/ipv-authorize.ts
+++ src/main/ipv-stub/ipv-authorize.ts
@@ -1,10 +1,13 @@
 import {
   APIGatewayProxyEvent,
+  APIGatewayProxyEventHeaders,
   APIGatewayProxyResult,
   Handler,
 } from "aws-lambda";
 import { logger } from "../logger";
 import { renderPage } from "../template";
+import { UserIdentity } from "./entity/user-identity";
+import { importPKCS8, compactDecrypt } from "jose";
 
 export const handler: Handler = async (
   event: APIGatewayProxyEvent
@@ -15,30 +18,80 @@ export const handler: Handler = async (
     case "POST":
       return post(event);
     default:
-      logger.error("Method not allowed");
-      return {
-        statusCode: 405,
-        body: JSON.stringify({
-          message: "Method not allowed",
-        }),
-      };
+      return errorResponse(405, "Method not allowed");
   }
 };
 
-function get(_event: APIGatewayProxyEvent): APIGatewayProxyResult {
+async function get(
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> {
   logger.info("Reached the GET endpoint");
+
+  const headers: APIGatewayProxyEventHeaders = event.headers;
+  const bearerToken = headers["Authorization"];
+  if (!bearerToken) {
+    return errorResponse(400, "Bearer token not found");
+  }
+  const encryptedJwt = bearerToken.split(" ")[1];
+
+  const ipvPrivateKeyPem = process.env.IPV_PRIVATE_KEY;
+  if (!ipvPrivateKeyPem) {
+    return errorResponse(500, "Private key not found");
+  }
+  const privateKey = await importPKCS8(ipvPrivateKeyPem, "RSA-OAEP-256");
+
+  let encodedJwt = "";
+  try {
+    const { plaintext } = await compactDecrypt(encryptedJwt, privateKey);
+    encodedJwt = plaintext.toString();
+  } catch (error) {
+    return errorResponse(500, "Decryption failed");
+  }
+
+  const parts = encodedJwt.split(".");
+  if (parts.length !== 3) {
+    return errorResponse(400, "Decrypted JWT is in invalid format");
+  }
+
+  let decodedHeader, decodedPayload, decodedSignature;
+  try {
+    [decodedHeader, decodedPayload, decodedSignature] = parts.map((part) =>
+      Buffer.from(part, "base64url").toString("utf8")
+    );
+  } catch (error) {
+    return errorResponse(500, "Decoding failed");
+  }
+
   return {
     statusCode: 200,
     headers: { "Content-Type": "text/html" },
     body: renderPage(
       `<h1 class="govuk-heading-l">Hello World from the IPV authorize lambda</h1>
-  <p class="govuk-body">This page will show the decrypted JAR</p>
-  <h4 class="govuk-body">This page will also contain the form to submit what you want for the IPV response. On submit it will send a POST request to the authorize lambda.</h4>
+  <h3 class="govuk-heading-s">Decrypted JAR header:</h3>
+  <p class="govuk-body">${JSON.parse(decodedHeader)}</p>
+  <h3 class="govuk-heading-s">Decrypted JAR payload:</h3>
+  <p class="govuk-body">${JSON.parse(decodedPayload)}</p>
+  <h3 class="govuk-heading-s">Form:</h3>
+  <p class="govuk-body">This page will also contain the form to submit what you want for the IPV response. On submit it will send a POST request to the authorize lambda.</p>
   <form action="" method="post">
     <button name="continue" value="continue" class="govuk-button">Continue</button>
   </form>`
     ),
   };
+}
+
+function errorResponse(
+  statusCode: number,
+  message: string
+): Promise<APIGatewayProxyResult> {
+  logger.error(message);
+  const result: APIGatewayProxyResult = {
+    statusCode: statusCode,
+    body: JSON.stringify({
+      message: message,
+    }),
+  };
+  return Promise.resolve(result);
 }
 
 function post(event: APIGatewayProxyEvent): APIGatewayProxyResult {
