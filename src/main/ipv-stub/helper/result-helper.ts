444d4eff1b5ba7d2e26efcd8f111f0d4c84f82c2
BenjaminWCO <benjamin.woolley@digital.cabinet-office.gov.uk>
2024-08-16 14:18:25 +0100

    ATO-862: Refactor Error Handling

---
+++ src/main/ipv-stub/helper/result-helper.ts
@@ -0,0 +1,79 @@
+import { APIGatewayProxyResult } from "aws-lambda";
+import { logger } from "../../logger";
+
+type SuccessCode = 200 | 302;
+type ErrorCode = 400 | 405 | 500;
+type JsonEntity =
+  | string
+  | number
+  | boolean
+  | null
+  | undefined
+  | object
+  | JsonEntity[];
+type Headers = { [header: string]: boolean | number | string };
+
+export class CodedError extends Error {
+  public code: ErrorCode;
+
+  constructor(code: ErrorCode, message: string) {
+    super(message);
+    this.code = code;
+  }
+}
+
+export function successfulJsonResult(
+  code: SuccessCode,
+  body: JsonEntity,
+  headers?: Headers | undefined
+): APIGatewayProxyResult {
+  return {
+    statusCode: code,
+    headers: { ...headers, "Content-Type": "application/json" },
+    body: JSON.stringify(body),
+  };
+}
+
+export function successfulHtmlResult(
+  code: SuccessCode,
+  body: string,
+  headers?: Headers | undefined
+): APIGatewayProxyResult {
+  return {
+    statusCode: code,
+    headers: { ...headers, "Content-Type": "text/html" },
+    body: body,
+  };
+}
+
+export function methodNotAllowedError(method: string) {
+  return new CodedError(405, `Method ${method} not allowed`);
+}
+
+export async function handleErrors(
+  getResult: () => Promise<APIGatewayProxyResult>
+): Promise<APIGatewayProxyResult> {
+  try {
+    return await getResult();
+  } catch (error) {
+    if (error instanceof CodedError) {
+      logger.error(error.message);
+      return {
+        statusCode: error.code,
+        body: JSON.stringify({
+          message: error.message,
+        }),
+      };
+    }
+
+    const errorStr =
+      error instanceof Error ? error.message : JSON.stringify(error);
+    logger.error(errorStr);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: `Encountered an unhandled exception: ${errorStr}`,
+      }),
+    };
+  }
+}
--- src/main/ipv-stub/ipv-user-identity.ts
+++ src/main/ipv-stub/ipv-user-identity.ts
@@ -4,32 +4,25 @@ import {
   Handler,
 } from "aws-lambda";
 import { USER_IDEINTITY } from "./data/ipv-dummy-constants";
+import {
+  handleErrors,
+  methodNotAllowedError,
+  successfulJsonResult,
+} from "./helper/result-helper";
 
 export const handler: Handler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  switch (event.httpMethod) {
-    case "GET":
-      return get();
-    default:
-      return {
-        statusCode: 405,
-        body: JSON.stringify({
-          message: "Method not allowed",
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      };
-  }
+  return handleErrors(async () => {
+    switch (event.httpMethod) {
+      case "GET":
+        return await get();
+      default:
+        throw methodNotAllowedError(event.httpMethod);
+    }
+  });
 };
 
-function get(): APIGatewayProxyResult {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(USER_IDEINTITY),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
+function get(): Promise<APIGatewayProxyResult> {
+  return Promise.resolve(successfulJsonResult(200, USER_IDEINTITY));
 }
