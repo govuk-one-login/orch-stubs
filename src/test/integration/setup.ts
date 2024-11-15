021b6778b6941e41058f3932495182bb5b7e6c80
Ethan Mills <ethan.mills@digital.cabinet-office.gov.uk>
2024-11-15 17:50:39 +0000

    ATO-871: Set AWS credentials in integration tests

--- src/test/integration/setup.ts
+++ src/test/integration/setup.ts
@@ -2,3 +2,6 @@ const queuename = "local-queue";
 process.env.QUEUE_NAME = queuename;
 process.env.LOCALSTACK_ENDPOINT = "http://localhost:4566";
 process.env.DESTINATION_QUEUE_URL = `http://localhost:4566/000000000000/${queuename}`;
+process.env.AWS_REGION = "eu-west-2";
+process.env.AWS_ACCESS_KEY_ID = "test-key";
+process.env.AWS_SECRET_ACCESS_KEY = "test-secret";
