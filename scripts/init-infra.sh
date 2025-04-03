cbb8eb3d25a7ca3dcb4e89de826bb38b059e5aab
Isaac Au <isaac.au@digital.cabinet-office.gov.uk>
2025-04-03 16:11:20 +0100

    ATO-1538: update localstack script with new tables

--- scripts/init-infra.sh
+++ scripts/init-infra.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 awslocal dynamodb create-table --table-name local-IpvStub-UserIdentity --key-schema AttributeName=UserIdentityId,KeyType=HASH --attribute-definitions AttributeName=UserIdentityId,AttributeType=S --billing-mode PAY_PER_REQUEST --region eu-west-2
+awslocal dynamodb create-table --table-name localAuthStubAuthCode --key-schema AttributeName=AuthCode,KeyType=HASH --attribute-definitions AttributeName=AuthCode,AttributeType=S --billing-mode PAY_PER_REQUEST --region eu-west-2
+awslocal dynamodb create-table --table-name localAuthStubToken --key-schema AttributeName=Token,KeyType=HASH --attribute-definitions AttributeName=Token,AttributeType=S --billing-mode PAY_PER_REQUEST --region eu-west-2
