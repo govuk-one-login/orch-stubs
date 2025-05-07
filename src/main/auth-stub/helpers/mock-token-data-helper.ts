a5228ebced25f7c23990d47efb88a31d81889756
Isaac Au <isaac.au@digital.cabinet-office.gov.uk>
2025-05-07 16:48:49 +0100

    ATO-1543: add mocking data in lambda  - mocking the data we save into dynamo from the time being as we have not implemented    sources for where all the data will come from.

---
+++ src/main/auth-stub/helpers/mock-token-data-helper.ts
@@ -0,0 +1,22 @@
+import { AccessTokenStoreInput } from "../interfaces/access-token-store-interface";
+
+const MOCK_SUBJECT_ID = "123456";
+const MOCK_CLAIMS = ["claim1"];
+const MOCK_SECTOR_IDENTIFIER = "9876543";
+const MOCK_IS_NEW_ACCOUNT = false;
+const MOCK_PASSWORD_RESET_TIME = 12;
+const MOCK_HAS_BEEN_USED = false;
+
+export const createAccessTokenStoreInput = (
+  accessToken: string
+): AccessTokenStoreInput => {
+  return {
+    accessToken: accessToken,
+    subjectId: MOCK_SUBJECT_ID,
+    claims: MOCK_CLAIMS,
+    sectorIdentifier: MOCK_SECTOR_IDENTIFIER,
+    isNewAccount: MOCK_IS_NEW_ACCOUNT,
+    passwordResetTime: MOCK_PASSWORD_RESET_TIME,
+    hasBeenUsed: MOCK_HAS_BEEN_USED,
+  };
+};
