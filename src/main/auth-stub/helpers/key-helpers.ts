cafd915f5a6b2ca5529b17d49b8c103b82fd9e4d
Louisa Nash <louisa.nash@digital.cabinet-office.gov.uk>
2025-05-13 14:22:56 +0100

    ATO-1536: add public/encryption key helper to auth stub

---
+++ src/main/auth-stub/helpers/key-helpers.ts
@@ -0,0 +1,42 @@
+import { logger } from "../../../main/logger";
+import { getEnv } from "./env-helper";
+import {
+  DecryptCommandInput,
+  DecryptCommandOutput,
+  EncryptionAlgorithmSpec,
+  KMS,
+} from "@aws-sdk/client-kms";
+import { base64DecodeToUint8Array } from "./encoding";
+import { getAwsRegion, getKmsKeyId } from "./config";
+
+export const getContentEncryptionKey = async (
+  encryptedKey: string
+): Promise<Uint8Array> => {
+  const localEncryptionKey = getEnv("DUMMY_ENCRYPTION_PUBLIC_KEY", false);
+  if (localEncryptionKey) {
+    logger.info(
+      "Found DUMMY_ENCRYPTION_PUBLIC_KEY env variable. Using value as encryption key source"
+    );
+    const localEncryptionKeyArray: Uint8Array = JSON.parse(localEncryptionKey);
+    return Promise.resolve(localEncryptionKeyArray);
+  } else {
+    const kmsClient = new KMS({
+      region: getAwsRegion(),
+    });
+    const encryptionAlgorithm = EncryptionAlgorithmSpec.RSAES_OAEP_SHA_256;
+    const kmsKeyId = getKmsKeyId();
+    const inputs: DecryptCommandInput = {
+      CiphertextBlob: base64DecodeToUint8Array(encryptedKey),
+      EncryptionAlgorithm: encryptionAlgorithm,
+      KeyId: kmsKeyId,
+    };
+
+    const decryptResponse: DecryptCommandOutput =
+      await kmsClient.decrypt(inputs);
+    return decryptResponse.Plaintext!;
+  }
+};
+
+export const getAuthPublicKey = (): string => {
+  return getEnv("AUTH_AUTHORIZE_PUBLIC_ENCRYPTION_KEY");
+};
