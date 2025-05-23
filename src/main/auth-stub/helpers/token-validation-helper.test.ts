9cfcce362d9c276c5b0faddb97df32c8dedf06c3
Isaac Au <isaac.au@digital.cabinet-office.gov.uk>
2025-05-23 09:07:29 +0100

    ATO-1544: add validation for plain text params  - grant type  - redirect uri  - client Id  - unit test for validation

--- src/main/auth-stub/helpers/token-validation-helper.test.ts
+++ src/main/auth-stub/helpers/token-validation-helper.test.ts
@@ -3,7 +3,10 @@ import {
   createAuthCodeStoreThatHasBeenUsed,
   createAuthCodeStoreThatHasExpired,
 } from "../test-helper/mock-auth-code-data-helper";
-import { validateAuthCode } from "./token-validation-helper";
+import {
+  validateAuthCode,
+  validatePlainTextParameters,
+} from "./token-validation-helper";
 import * as authCodeDynamoDbService from "../services/auth-code-dynamodb-service";
 
 describe("Token Validation Helper", () => {
@@ -41,6 +44,93 @@ describe("Token Validation Helper", () => {
 
       await expect(action()).rejects.toThrow(
         "Invalid Auth Code: already in use"
+      );
+    });
+  });
+
+  describe("validate plain text parameters", () => {
+    const redirectUri = "testUri";
+    const clientId = "testClientId";
+
+    let tokenRequestBody: { [k: string]: string };
+
+    beforeEach(() => {
+      tokenRequestBody = {
+        grant_type: "authorization_code",
+        redirect_uri: redirectUri,
+        clientId: clientId,
+      };
+    });
+
+    it("should error when the request-parameters does not exist", () => {
+      const action = () =>
+        validatePlainTextParameters(redirectUri, clientId, undefined);
+
+      expect(action).toThrow("Request requires query parameters");
+    });
+
+    it("should error when the request-parameters are empty", () => {
+      const action = () =>
+        validatePlainTextParameters(redirectUri, clientId, {});
+
+      expect(action).toThrow("Request requires query parameters");
+    });
+
+    it("should error when the grant-type does not exist", () => {
+      delete tokenRequestBody["grant_type"];
+
+      const action = () =>
+        validatePlainTextParameters(redirectUri, clientId, tokenRequestBody);
+
+      expect(action).toThrow("Request is missing grant_type parameter");
+    });
+
+    it("should error when the grant-type is not valid", () => {
+      tokenRequestBody["grant_type"] = "incorrect_grant_type";
+
+      const action = () =>
+        validatePlainTextParameters(redirectUri, clientId, tokenRequestBody);
+
+      expect(action).toThrow("Request has invalid grant_type parameter");
+    });
+
+    it("should error when the redirect-uri does not exist", () => {
+      delete tokenRequestBody["redirect_uri"];
+
+      const action = () =>
+        validatePlainTextParameters(redirectUri, clientId, tokenRequestBody);
+
+      expect(action).toThrow("Request is missing redirect_uri parameter");
+    });
+
+    it("should error when the redirect-uri is not valid", () => {
+      tokenRequestBody["redirect_uri"] = "incorrect_redirect_uri";
+
+      const action = () =>
+        validatePlainTextParameters(redirectUri, clientId, tokenRequestBody);
+
+      expect(action).toThrow(
+        "Request redirect_uri is not the permitted redirect_uri"
+      );
+    });
+
+    it("should error when the client-id is does not exist", () => {
+      delete tokenRequestBody["client_id"];
+
+      const action = () =>
+        validatePlainTextParameters(redirectUri, clientId, tokenRequestBody);
+
+      expect(action).toThrow("Request is missing client_id parameter");
+    });
+
+    it("should error when the client-id is not valid", () => {
+      tokenRequestBody["client_id"] = "incorrect_client_id";
+
+      const action = () =>
+        validatePlainTextParameters(redirectUri, clientId, tokenRequestBody);
+
+      expect(action).toThrow(
+        "Request client_id is not the permitted client_id"
       );
     });
   });
