6f13a5dc81161e8c2c60a53528138fa9080cf2f1
Isaac Au <isaac.au@digital.cabinet-office.gov.uk>
2025-05-23 09:07:29 +0100

    ATO-1544: add validation for plain text params  - grant type  - redirect uri  - client Id  - unit test for validation

--- src/main/auth-stub/helpers/token-validation-helper.ts
+++ src/main/auth-stub/helpers/token-validation-helper.ts
@@ -16,3 +16,47 @@ export const validateAuthCode = async (authCode: string | undefined) => {
     throw new CodedError(400, "Invalid Auth Code: expired");
   }
 };
+
+export const validatePlainTextParameters = (
+  redirectUri: string,
+  clientId: string,
+  body:
+    | {
+        [k: string]: string;
+      }
+    | undefined
+) => {
+  if (body === undefined || Object.keys(body).length === 0) {
+    throw new CodedError(400, "Request requires query parameters");
+  }
+
+  if (!body["grant_type"]) {
+    throw new CodedError(400, "Request is missing grant_type parameter");
+  }
+
+  if (body["grant_type"] !== "authorization_code") {
+    throw new CodedError(400, "Request has invalid grant_type parameter");
+  }
+
+  if (!body["redirect_uri"]) {
+    throw new CodedError(400, "Request is missing redirect_uri parameter");
+  }
+
+  if (body["redirect_uri"] !== redirectUri) {
+    throw new CodedError(
+      400,
+      "Request redirect_uri is not the permitted redirect_uri"
+    );
+  }
+
+  if (!body["client_id"]) {
+    throw new CodedError(400, "Request is missing client_id parameter");
+  }
+
+  if (body["client_id"] !== clientId) {
+    throw new CodedError(
+      400,
+      "Request client_id is not the permitted client_id"
+    );
+  }
+};
