2514da15889a4e4466f7f80931fc1aae76a0f9f4
Isaac Au <isaac.au@digital.cabinet-office.gov.uk>
2025-05-29 16:14:32 +0100

    ATO-1547: add user profile table to docker script

--- scripts/init-infra.sh
+++ scripts/init-infra.sh
@@ -3,3 +3,14 @@
 awslocal dynamodb create-table --table-name local-IpvStub-UserIdentity --key-schema AttributeName=UserIdentityId,KeyType=HASH --attribute-definitions AttributeName=UserIdentityId,AttributeType=S --billing-mode PAY_PER_REQUEST --region eu-west-2
 awslocal dynamodb create-table --table-name local-AuthStub-AuthCode --key-schema AttributeName=authCode,KeyType=HASH --attribute-definitions AttributeName=authCode,AttributeType=S --billing-mode PAY_PER_REQUEST --region eu-west-2
 awslocal dynamodb create-table --table-name local-AuthStub-AccessToken --key-schema AttributeName=accessToken,KeyType=HASH --attribute-definitions AttributeName=accessToken,AttributeType=S --billing-mode PAY_PER_REQUEST --region eu-west-2
+awslocal dynamodb create-table --table-name local-AuthStub-UserProfile --key-schema AttributeName=email,KeyType=HASH --attribute-definitions AttributeName=email,AttributeType=S AttributeName=subjectId,AttributeType=S --global-secondary-indexes \
+          "[
+              {
+                \"IndexName\": \"SubjectIdIndex\",
+                \"KeySchema\": [
+                  { \"AttributeName\": \"subjectId\", \"KeyType\": \"HASH\"}
+                ],
+                \"Projection\": {\"ProjectionType\":\"ALL\"}
+              }
+          ]" \
+  --billing-mode PAY_PER_REQUEST --region eu-west-2
