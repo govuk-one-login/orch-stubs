618289964e4b870b2aa3687d6194dba58555c63e
Louisa Nash <louisa.nash@digital.cabinet-office.gov.uk>
2025-05-29 14:43:46 +0100

    ATO-1545: add get access token from authorization header

--- src/main/auth-stub/helpers/request-header-helper.test.ts
+++ src/main/auth-stub/helpers/request-header-helper.test.ts
@@ -1,27 +1,77 @@
 import { APIGatewayProxyEventHeaders } from "aws-lambda";
-import { getHeaderValueFromHeaders } from "./request-header-helper";
+import {
+  getAccessTokenFromAuthorizationHeader,
+  getHeaderValueFromHeaders,
+} from "./request-header-helper";
 
-it("should return header value from header", () => {
-  const headers: APIGatewayProxyEventHeaders = { header: "header value" };
-  const headerValue = getHeaderValueFromHeaders(headers, "header");
-  expect(headerValue).toBe("header value");
-});
+describe("Request header helper", () => {
+  describe("getHeaderValueFromHeaders tests", () => {
+    it("should return header value from header", () => {
+      const headers: APIGatewayProxyEventHeaders = { header: "header value" };
 
-test("should return null when header cannot be found in headers", () => {
-  const headers: APIGatewayProxyEventHeaders = { header: "header value" };
-  const headerValue = getHeaderValueFromHeaders(headers, "new-header");
-  expect(headerValue).toBe(null);
-});
+      const headerValue = getHeaderValueFromHeaders(headers, "header");
 
-describe("matchCase tests", () => {
-  const headers: APIGatewayProxyEventHeaders = { header: "header value" };
-  test("should return header value from header with matchCase set to true", () => {
-    const headerValue = getHeaderValueFromHeaders(headers, "HEADER", true);
-    expect(headerValue).toBe("header value");
+      expect(headerValue).toBe("header value");
+    });
+
+    it("should return null when header cannot be found in headers", () => {
+      const headers: APIGatewayProxyEventHeaders = { header: "header value" };
+
+      const headerValue = getHeaderValueFromHeaders(headers, "new-header");
+
+      expect(headerValue).toBe(null);
+    });
+
+    describe("matchCase tests", () => {
+      it("should return header value from header with matchCase set to true", () => {
+        const headers: APIGatewayProxyEventHeaders = { header: "header value" };
+
+        const headerValue = getHeaderValueFromHeaders(headers, "HEADER", true);
+
+        expect(headerValue).toBe("header value");
+      });
+
+      it("should return null when matchCase set to false", () => {
+        const headers: APIGatewayProxyEventHeaders = { header: "header value" };
+
+        const headerValue = getHeaderValueFromHeaders(headers, "HEADER", false);
+
+        expect(headerValue).toBe(null);
+      });
+    });
   });
 
-  it("should return null when matchCase set to false", () => {
-    const headerValue = getHeaderValueFromHeaders(headers, "HEADER", false);
-    expect(headerValue).toBe(null);
+  describe("getAccessTokenFromAuthorizationHeader tests", () => {
+    it("should return header value from header with valid header", () => {
+      const header = "bearer testValue";
+
+      const headerValue = getAccessTokenFromAuthorizationHeader(header);
+
+      expect(headerValue).toBe("testValue");
+    });
+
+    it("should return null when header doesn't begin with bearer", () => {
+      const header = "testValue bearer";
+
+      const action = () => getAccessTokenFromAuthorizationHeader(header);
+
+      expect(action).toThrow("Unable to extract (opaque) bearer token");
+    });
+
+    it("should return null when length is not 2", () => {
+      const header = "bearer test value";
+
+      const action = () => getAccessTokenFromAuthorizationHeader(header);
+
+      expect(action).toThrow("Unable to extract (opaque) bearer token");
+    });
+
+    it("should return null when second half is empty", () => {
+      const header = "bearer ";
+
+      const action = () => getAccessTokenFromAuthorizationHeader(header);
+
+      expect(action).toThrow("Unable to extract (opaque) bearer token");
+    });
   });
 });
