aeeb6b098ab293fe099d24be128c2fe36bb81195
Isaac Au <isaac.au@digital.cabinet-office.gov.uk>
2025-06-10 09:11:11 +0100

    ATO-1706: add temporary user profile in authorize  - add userprofile in service  - mock user profile  - test in authorize

--- src/main/auth-stub/helpers/mock-token-data-helper.ts
+++ src/main/auth-stub/helpers/mock-token-data-helper.ts
@@ -1,4 +1,5 @@
 import { AccessTokenStoreInput } from "../interfaces/access-token-store-interface";
+import { UserProfile } from "../interfaces/user-profile-interface";
 
 const MOCK_SUBJECT_ID = "123456";
 const MOCK_CLAIMS = ["claim1"];
@@ -6,6 +7,21 @@ const MOCK_SECTOR_IDENTIFIER = "9876543";
 const MOCK_IS_NEW_ACCOUNT = false;
 const MOCK_PASSWORD_RESET_TIME = 12;
 const MOCK_HAS_BEEN_USED = false;
+
+const MOCK_EMAIL_VERIFIED = true;
+const MOCK_PHONE_NUMBER = "07123098567";
+const MOCK_PHONE_NUMBER_VERIFIED = true;
+const MOCK_CREATED = "2025-04-11T12:00:00";
+const MOCK_UPDATED = "2025-04-11T12:00:00";
+const MOCK_TERMS_AND_CONDITIONS = {
+  version: "1.0",
+  timeStamp: "2025-04-10T12:00:00",
+};
+const MOCK_PUBLIC_SUBJECT_ID = "public-subject-id";
+const MOCK_LEGACY_SUBJECT_ID = "legacy-subject-id";
+const MOCK_SALT = "salt";
+const MOCK_ACCOUNT_VERIFIED = 1;
+const MOCK_TEST_USER = 0;
 
 export const createAccessTokenStoreInput = (
   accessToken: string
@@ -18,5 +34,23 @@ export const createAccessTokenStoreInput = (
     isNewAccount: MOCK_IS_NEW_ACCOUNT,
     passwordResetTime: MOCK_PASSWORD_RESET_TIME,
     hasBeenUsed: MOCK_HAS_BEEN_USED,
+  };
+};
+
+export const createUserPofile = (email: string): UserProfile => {
+  return {
+    subjectId: MOCK_SUBJECT_ID,
+    email: email,
+    emailVerified: MOCK_EMAIL_VERIFIED,
+    phoneNumber: MOCK_PHONE_NUMBER,
+    phoneNumberVerified: MOCK_PHONE_NUMBER_VERIFIED,
+    created: MOCK_CREATED,
+    updated: MOCK_UPDATED,
+    termsAndConditions: MOCK_TERMS_AND_CONDITIONS,
+    publicSubjectId: MOCK_PUBLIC_SUBJECT_ID,
+    legacySubjectId: MOCK_LEGACY_SUBJECT_ID,
+    salt: MOCK_SALT,
+    accountVerified: MOCK_ACCOUNT_VERIFIED,
+    testUser: MOCK_TEST_USER,
   };
 };
--- src/main/auth-stub/services/user-profile-dynamodb-service.ts
+++ src/main/auth-stub/services/user-profile-dynamodb-service.ts
@@ -35,3 +35,24 @@ export const getUserProfileBySubjectId = async (
   });
   return response.Items![0] as UserProfile;
 };
+
+export const addUserProfile = async (userProfile: UserProfile) => {
+  return await dynamo.put({
+    TableName: tableName,
+    Item: {
+      subjectId: userProfile.subjectId,
+      email: userProfile.email,
+      emailVerified: userProfile.emailVerified,
+      phoneNumber: userProfile.phoneNumber,
+      phoneNumberVerified: userProfile.phoneNumberVerified,
+      created: userProfile.created,
+      updated: userProfile,
+      termsAndConditions: userProfile.termsAndConditions,
+      publicSubjectID: userProfile.publicSubjectId,
+      legacySubjectID: userProfile.legacySubjectId,
+      salt: userProfile.salt,
+      accountVerified: userProfile.accountVerified,
+      testUser: userProfile.testUser,
+    },
+  });
+};
