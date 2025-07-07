c3d160c0771a702db28622d56c85893b2b9b8e6e
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2025-07-07 14:02:35 +0100

    ATO-1795: Adds oAuth error fields and client side js
    Adds some simple (if not hacky) client side JS which uses the
    new dropdown to specify if we return an oAuth error back from IPV.
    
    I've made it such that it removes fields from form if we choose to
    return an oAuth error and brings them back if we choose not to.

--- src/main/ipv-stub/render-ipv-authorize.ts
+++ src/main/ipv-stub/render-ipv-authorize.ts
@@ -78,12 +78,22 @@ export default function renderIPVAuthorize(
   
   <h3 class="govuk-heading-s">Form:</h3>
   <p class="govuk-body">Use this form to configure the required IPV user identity response. On submit a POST request will be sent to /authorize and the IPV OAuth 2.0 flow will be initiated.</p>
+
+  <div class="govuk-form-group">
+  <label class="govuk-label" for="form-success">
+    oAuth Error
+  </label>
+  <select class="govuk-select" id="form-success" name="form-success">
+    <option selected="selected" value="none">None</option>
+    <option value="oAuthError">Error</option>
+  </select>
+</div>
   <form action="/authorize" method="post">
    <input type="hidden" name="authCode" value=${authCode}>
 
   <dl class="govuk-summary-list">
 
-  <div class="govuk-summary-list__row">
+  <div class="govuk-summary-list__row" id="sub-claim-row">
   <dt class="govuk-summary-list__key">
       Subject Claim (sub)
   </dt>
@@ -92,7 +102,7 @@ export default function renderIPVAuthorize(
   </dd>
 </div>
 
-<div class="govuk-summary-list__row">
+<div class="govuk-summary-list__row" id="vtr-claim-row">
 <dt class="govuk-summary-list__key">
 Vector of trust claim (vot)
 </dt>
@@ -101,7 +111,7 @@ Vector of trust claim (vot)
 </dd>
 </div>
 
-<div class="govuk-summary-list__row">
+<div class="govuk-summary-list__row" id="vtm-claim-row">
 <dt class="govuk-summary-list__key">
 Trustmark claim (vtm)
 </dt>
@@ -110,7 +120,7 @@ Trustmark claim (vtm)
 </dd>
 </div>
 
-  <div class="govuk-summary-list__row">
+  <div class="govuk-summary-list__row" id="identity-claim-row">
     <dt class="govuk-summary-list__key">
         CoreIdentity Claim
     </dt>
@@ -121,7 +131,7 @@ Trustmark claim (vtm)
 
   ${
     claimKeys.includes("https://vocab.account.gov.uk/v1/address")
-      ? `<div class="govuk-summary-list__row">
+      ? `<div class="govuk-summary-list__row" id="address-claim-row">
   <dt class="govuk-summary-list__key">
       Address Claim
   </dt>
@@ -134,7 +144,7 @@ Trustmark claim (vtm)
 
   ${
     claimKeys.includes("https://vocab.account.gov.uk/v1/passport")
-      ? `<div class="govuk-summary-list__row">
+      ? `<div class="govuk-summary-list__row" id="passport-claim-row">
       <dt class="govuk-summary-list__key">
           Passport Claim
       </dt>
@@ -147,7 +157,7 @@ Trustmark claim (vtm)
 
   ${
     claimKeys.includes("https://vocab.account.gov.uk/v1/drivingPermit")
-      ? ` <div class="govuk-summary-list__row">
+      ? ` <div class="govuk-summary-list__row" id="driving-permit-record-row">
       <dt class="govuk-summary-list__key">
           Driving Permit Claim
       </dt>
@@ -160,7 +170,7 @@ Trustmark claim (vtm)
   
   ${
     claimKeys.includes("https://vocab.account.gov.uk/v1/socialSecurityRecord")
-      ? ` <div class="govuk-summary-list__row">
+      ? ` <div class="govuk-summary-list__row" id="social-security-record-row">
       <dt class="govuk-summary-list__key">
           Social Security Record Claim
       </dt>
@@ -171,15 +181,73 @@ Trustmark claim (vtm)
       : ""
   }
  
-  <div class="govuk-summary-list__row">
+  <div class="govuk-summary-list__row" id="return-code-row">
     <dt class="govuk-summary-list__key">
         Return Code Claim
     </dt>
     <dd class="govuk-summary-list__value" id="user-info-return-code-claim-present">
     <textarea class="govuk-textarea" rows="8" id="return_code_claim" name="return_code_claim" type="text">${JSON.stringify(config.returnCode, null, 2)}</textarea>
     </dd>
+    </div>
+
+    <div class="govuk-summary-list__row" id="oAuthErrorRow">
+    <dt class="govuk-summary-list__key">
+        oAuth Error
+    </dt>
+    <dd class="govuk-summary-list__value" id="oAuthError">
+    <textarea class="govuk-textarea" rows="2" id="oAuthError" name="oAuthError" type="text"></textarea>
+    </dd>
+      </div>
+
+    <div class="govuk-summary-list__row" id="oAuthErrorDescriptionRow">
+    <dt class="govuk-summary-list__key">
+        oAuth Error Description
+    </dt>
+    <dd class="govuk-summary-list__value" id="oAuthErrorDescriptionId">
+    <textarea class="govuk-textarea" rows="2" id="oAuthErrorDescription" name="oAuthErrorDescription" type="text"></textarea>
+    </dd>
   </div>
     <button name="continue" value="continue" class="govuk-button">Continue</button>
-  </form>`
+  </form>`,
+    `
+  const dropdown = document.getElementById("form-success")
+  const oAuthErrorInput = document.getElementById("oAuthErrorRow")
+  const oAuthErrorDesc = document.getElementById("oAuthErrorDescriptionRow")
+  const claim_values= ["sub-claim-row",
+  "vtr-claim-row",
+  "vtm-claim-row", 
+  "identity-claim-row", 
+  "passport-claim-row", 
+  "address-claim-row",
+  "driving-permit-record-row",
+  "social-security-record-row",
+  "return-code-row"
+   ]
+
+    oAuthErrorInput.classList.add("hidden")
+    oAuthErrorDesc.classList.add("hidden")
+
+
+  dropdown.addEventListener("change", () => {
+    const val = dropdown.value
+    if(val === "none"){
+      oAuthErrorInput.classList.add("hidden")
+      oAuthErrorDesc.classList.add("hidden")
+
+      claim_values.forEach(claimField => {
+        const claimElement = document.getElementById(claimField)
+        claimElement?.classList?.remove("hidden")
+      })
+
+    } else if (val === "oAuthError"){
+      claim_values.forEach(claimField => {
+        const claimElement = document.getElementById(claimField)
+        claimElement?.classList?.add("hidden")
+      })
+
+      oAuthErrorInput.classList.remove("hidden")
+      oAuthErrorDesc.classList.remove("hidden")
+    }
+  })`
   );
 }
