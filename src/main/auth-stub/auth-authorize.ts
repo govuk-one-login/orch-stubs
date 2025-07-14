f612682abe72579532b4f5c1b0c8c73ae97c8c3d
Louisa Nash <louisa.nash@digital.cabinet-office.gov.uk>
2025-07-14 16:44:57 +0100

    tmp

--- src/main/auth-stub/auth-authorize.ts
+++ src/main/auth-stub/auth-authorize.ts
@@ -17,10 +17,14 @@ import { Claims } from "./helpers/claims-config";
 import {
   CodedError,
   handleErrors,
+  successfulHtmlResult,
   successfulJsonResult,
 } from "../helper/result-helper";
 import { ROOT_URI } from "./data/auth-dummy-constants";
 import { createUserPofile } from "./helpers/mock-token-data-helper";
+import renderAuthAuthorize from "./render-auth-authorize";
+import { logger } from "../logger";
+import { compactDecrypt, jwtVerify } from "jose";
 
 export const handler: Handler = async (
   event: APIGatewayProxyEvent
@@ -45,17 +49,52 @@ export const handler: Handler = async (
 async function get(
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> {
+  logger.info("Auth Authorize GET endpoint invoked!");
+
+  if (event.queryStringParameters == null) {
+    throw new CodedError(400, "Query string parameters are null");
+  }
+  const requestObject = event.queryStringParameters["request"] as string;
+
+  if (!requestObject) {
+    throw new CodedError(400, "Request query string parameter not found");
+  }
+
+  const orchJwks = getOrchJwks();
+  const authPrivateKey = await getAuthPrivateKey();
+
+  const { plaintext } = await compactDecrypt(requestObject, authPrivateKey);
+  const encodedJwt = plaintext.toString();
+
+  const parts = encodedJwt.split(".");
+  if (parts.length !== 3) {
+    throw new CodedError(400, "Decrypted JWT is in invalid format");
+  }
+
+  let jwt;
+  try {
+    jwt = await jwtVerify(encodedJwt, orchJwks);
+  } catch (error) {
+    logger.error(
+      `Failed to verify client_assertion from orchestration: ${(error as Error).message}`
+    );
+    throw new CodedError(500, "Signature verification failed");
+  }
+
+  const header = jwt.protectedHeader;
+  const payload = jwt.payload;
+
+  const authCode = generateAuthCode();
+
   try {
     await addUserProfile(createUserPofile("dummy.email@mail.com"));
   } catch (error) {
     throw new CodedError(500, `dynamoDb error: ${error}`);
   }
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: `Reached the ${event.httpMethod} endpoint`,
-    }),
-  };
+  return successfulHtmlResult(
+    200,
+    renderAuthAuthorize(header, payload, authCode)
+  );
 }
 
 async function post(
@@ -75,6 +114,7 @@ async function post(
   const passwordResetTime = parsedBody.password_reset_time
     ? Number(parsedBody.password_reset_time)
     : 10;
+  const authCode = parsedBody["authCode"];
 
   let claims: Claims;
 
@@ -90,12 +130,9 @@ async function post(
     );
   }
 
-  let authCode: string;
-
   try {
     const user = await getUserProfileByEmail(email);
     const claimsList = claims.claim ? JSON.parse(claims.claim) : [];
-    authCode = generateAuthCode();
 
     const authCodeResult: AuthCodeStoreInput = {
       authCode,
---
+++ src/main/auth-stub/config/config.ts
@@ -0,0 +1,126 @@
+const expiryDate = new Date();
+expiryDate.setFullYear(expiryDate.getFullYear() + 1);
+
+export const getTrustmarkUri = (): string =>
+  `https://oidc.${process.env.ENVIRONMENT === "dev" ? "sandpit" : process.env.ENVIRONMENT}.account.gov.uk/trustmark`;
+
+export default {
+  coreIdentityJWT: {
+    sub: "urn:fdc:gov.uk:2022:56P4CMsGh_02YOlWpd8PAOI-2sVlB2nsNU7mcLZYhYw=",
+    iss: `https://identity.${process.env.ENVIRONMENT}.account.gov.uk/`,
+    aud: "YOUR_CLIENT_ID",
+    nbf: Date.now(),
+    iat: Date.now(),
+    exp: expiryDate.valueOf(),
+    vot: "P2",
+    vtm: getTrustmarkUri(),
+    vc: {
+      type: ["VerifiableCredential", "VerifiableIdentityCredential"],
+      credentialSubject: {
+        name: [
+          {
+            validFrom: "2020-03-01",
+            nameParts: [
+              {
+                value: "Alice",
+                type: "GivenName",
+              },
+              {
+                value: "Jane",
+                type: "GivenName",
+              },
+              {
+                value: "Laura",
+                type: "GivenName",
+              },
+              {
+                value: "Doe",
+                type: "FamilyName",
+              },
+            ],
+          },
+          {
+            validUntil: "2020-03-01",
+            nameParts: [
+              {
+                value: "Alice",
+                type: "GivenName",
+              },
+              {
+                value: "Jane",
+                type: "GivenName",
+              },
+              {
+                value: "Laura",
+                type: "GivenName",
+              },
+              {
+                value: "O’Donnell",
+                type: "FamilyName",
+              },
+            ],
+          },
+        ],
+        birthDate: [
+          {
+            value: "1970-01-01",
+          },
+        ],
+      },
+    },
+  },
+  address: [
+    {
+      uprn: "10022812929",
+      subBuildingName: "FLAT 5",
+      buildingName: "WEST LEA",
+      buildingNumber: "16",
+      dependentStreetName: "KINGS PARK",
+      streetName: "HIGH STREET",
+      doubleDependentAddressLocality: "EREWASH",
+      dependentAddressLocality: "LONG EATON",
+      addressLocality: "GREAT MISSENDEN",
+      postalCode: "HP16 0AL",
+      addressCountry: "GB",
+      validFrom: "2022-01-01",
+    },
+    {
+      uprn: "10002345923",
+      buildingName: "SAWLEY MARINA",
+      streetName: "INGWORTH ROAD",
+      dependentAddressLocality: "LONG EATON",
+      addressLocality: "NOTTINGHAM",
+      postalCode: "BH12 1JY",
+      addressCountry: "GB",
+      validUntil: "2022-01-01",
+    },
+  ],
+  drivingPermit: [
+    {
+      expiryDate: "2023-01-18",
+      issueNumber: "5",
+      issuedBy: "DVLA",
+      personalNumber: "DOE99802085J99FG",
+    },
+  ],
+  passport: [
+    {
+      documentNumber: "1223456",
+      icaoIssuerCode: "GBR",
+      expiryDate: "2032-02-02",
+    },
+  ],
+  returnCode: [
+    {
+      code: "EXAMPLE1",
+    },
+    {
+      code: "EXAMPLE2",
+    },
+  ],
+  socialSecurityRecord: [
+    {
+      personalNumber: "QQ123456C",
+    },
+  ],
+};
