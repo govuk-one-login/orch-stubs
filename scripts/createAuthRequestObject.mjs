bc50941d8069eb5055ab1d7c9bb59a13e3271b0b
Craig Earl <craig.earl@digital.cabinet-office.gov.uk>
2026-01-07 14:20:11 +0000

    ATO-2241: Add script to generate auth authorize request
    Co-authored-by: Isaac Au <isaac.au@digital.cabinet-office.gov.uk>

---
+++ scripts/createAuthRequestObject.mjs
@@ -0,0 +1,70 @@
+import { createPublicKey, randomUUID } from "crypto";
+import localParams from "../parameters.json" assert { type: "json" };
+import { CompactEncrypt, importPKCS8, SignJWT } from "jose";
+
+const publicEncryptionKey = createPublicKey(
+  localParams.Parameters.LOCAL_AUTH_AUTHORIZE_PRIVATE_ENCRYPTION_KEY
+);
+const privateSigningPem =
+  localParams.Parameters.LOCAL_AUTH_AUTHORIZE_PRIVATE_SIGNING_KEY;
+
+const main = async () => {
+  const privateSigningKey = await importPKCS8(privateSigningPem, "ES256");
+  const signedJwt = await new SignJWT({
+    sub: "urn:fdc:gov.uk:2022:Js1eJ0BbwPJEZIVV8DtXeLs-BSWHhKL-qHOjpnY7R-w",
+    response_type: "code",
+    govuk_signin_journey_id: "jAWCpUMvz6x7kTJTXTgC2OeldWM",
+    aud: "https://authstub.oidc.local.account.gov.uk",
+    vtr: ["P2"],
+    scope: "openid email phone",
+    state: randomUUID(),
+    jti: randomUUID(),
+    requested_credential_strength: "Cl.Cm",
+    iss: "https://authstub.oidc.local.account.gov.uk",
+    exp: 1768723848,
+    iat: 123456789,
+    nbf: 123456789,
+    client_name: "Orch",
+    cookie_consent_shared: true,
+    is_one_login_service: true,
+    service_type: "pairwise",
+    client_id: "orchestrationAuth",
+    redirect_uri: "https://www.google.com",
+    rp_sector_host: randomUUID(),
+    authenticated: true,
+    claims: {
+      userinfo: {
+        "https://vocab.account.gov.uk/v1/storageAccessToken": {
+          values: "storageAccessToken",
+        },
+        "https://vocab.account.gov.uk/v1/passport": {
+          essential: true,
+        },
+        "https://vocab.account.gov.uk/v1/socialSecurityRecord": {
+          essential: true,
+        },
+        "https://vocab.account.gov.uk/v1/drivingPermit": {
+          essential: true,
+        },
+        "https://vocab.account.gov.uk/v1/coreIdentityJWT": {
+          essential: true,
+        },
+        "https://vocab.account.gov.uk/v1/address": {
+          essential: true,
+        },
+        "https://vocab.account.gov.uk/v1/returnCode": {
+          essential: true,
+        },
+      },
+    },
+  })
+    .setProtectedHeader({ alg: "ES256" })
+    .sign(privateSigningKey);
+
+  const requestObject = await new CompactEncrypt(Buffer.from(signedJwt))
+    .setProtectedHeader({ alg: "RSA-OAEP-256", enc: "A256GCM" })
+    .encrypt(publicEncryptionKey);
+  return requestObject;
+};
+
+main().then((r) => console.log("Here is the request object: " + r));
