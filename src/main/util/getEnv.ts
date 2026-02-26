eabf7b49465b6a4ec01b3c172adf2801406ac130
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2026-02-26 17:19:18 +0000

    BAU: Add account intervention stub handler

--- src/main/util/getEnv.ts
+++ src/main/util/getEnv.ts
@@ -1,6 +1,6 @@
 import { AisStubEnvVar } from "../ais-stub/types/AisStubEnvVar";
 
-export type BaseEnvVar = "ENVIRONMENT";
+export type BaseEnvVar = "ENVIRONMENT" | "LOCALSTACK_ENDPOINT";
 
 export const getEnv = (name: BaseEnvVar | AisStubEnvVar): string => {
   if (!process.env[name]) {
