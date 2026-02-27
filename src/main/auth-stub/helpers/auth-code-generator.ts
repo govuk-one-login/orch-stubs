1aba1b6ff1fa87dff0af8f6e923382be9d6a2bf4
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2026-02-27 11:56:24 +0000

    ATO-2377: Fix reference error
    Sometimes the unit tests fail with a reference error.

--- src/main/auth-stub/helpers/auth-code-generator.ts
+++ src/main/auth-stub/helpers/auth-code-generator.ts
@@ -1,5 +1,7 @@
+import { getRandomValues } from "crypto";
+
 export const generateAuthCode = (): string => {
   const bytes = new Uint8Array(32);
-  crypto.getRandomValues(bytes);
+  getRandomValues(bytes);
   return Buffer.from(bytes).toString("base64");
 };
