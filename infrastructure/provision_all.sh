a035f65da95082fdae8e2fceb6e9c7ea78cf06e9
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2026-03-02 11:39:04 +0000

    ATO-2377: Update provision_all script for ais stub
    Updates this script to include the new AIS stub pipeline

--- infrastructure/provision_all.sh
+++ infrastructure/provision_all.sh
@@ -14,7 +14,7 @@ export TAGS_FILE
 ## Provision dependencies
 for dir in configuration/"$ENVIRONMENT"/*/; do
   STACK=$(basename "$dir")
-  if [[ $STACK != "$ENVIRONMENT-auth-stub-pipeline" && $STACK != "$ENVIRONMENT-ipv-stub-pipeline" && $STACK != "$ENVIRONMENT-spot-stub-pipeline"&& -f configuration/$ENVIRONMENT/$STACK/parameters.json ]]; then
+  if [[ $STACK != "$ENVIRONMENT-auth-stub-pipeline" && $STACK != "$ENVIRONMENT-ipv-stub-pipeline" && $STACK != "$ENVIRONMENT-spot-stub-pipeline" && $STACK != "$ENVIRONMENT-ais-stub-pipeline" && -f configuration/$ENVIRONMENT/$STACK/parameters.json ]]; then
     PARAMETERS_FILE="$(pwd)/configuration/${ENVIRONMENT}/${STACK}/parameters.json"
     export PARAMETERS_FILE
     $PROVISION_COMMAND "$ENVIRONMENT" "$STACK" "$STACK" LATEST &
@@ -24,7 +24,7 @@ done
 ## Provision secure pipelines
 for dir in configuration/"$ENVIRONMENT"/*/; do
   STACK=$(basename "$dir")
-  if [[ $STACK == "$ENVIRONMENT-auth-stub-pipeline" || $STACK == "$ENVIRONMENT-ipv-stub-pipeline" || $STACK == "$ENVIRONMENT-spot-stub-pipeline" ]]; then
+  if [[ $STACK == "$ENVIRONMENT-auth-stub-pipeline" || $STACK == "$ENVIRONMENT-ipv-stub-pipeline" || $STACK == "$ENVIRONMENT-spot-stub-pipeline" || $STACK == "$ENVIRONMENT-ais-stub-pipeline" ]]; then
     PARAMETERS_FILE="$(pwd)/configuration/${ENVIRONMENT}/${STACK}/parameters.json"
     export PARAMETERS_FILE
     $PROVISION_COMMAND "$ENVIRONMENT" "$STACK" sam-deploy-pipeline LATEST
