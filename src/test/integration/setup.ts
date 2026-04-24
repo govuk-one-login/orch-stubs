5c24f9ef51a2521e2fb796f8cc592589c7d1e8a0
Joe Edwards <joe.edwards@digital.cabinet-office.gov.uk>
2026-04-24 12:34:57 +0100

    BAU: Fix tests

--- src/test/integration/setup.ts
+++ src/test/integration/setup.ts
@@ -1,19 +1,25 @@
 import localParams from "./../../../parameters.json" with { type: "json" };
+
 const waitForLocalStack = async () => {
   let polls = 0;
 
   do {
-    const res = await fetch("http://127.0.0.1:4566/_localstack/health");
+    try {
+      const res = await fetch("http://127.0.0.1:4566/_localstack/health");
 
-    if (res.ok && res.status === 200) {
-      break;
+      if (res.ok && res.status === 200) {
+        break;
+      }
+    } catch (err) {
+      console.error((err as Error).message);
     }
+
     polls++;
 
     await new Promise((r) => setTimeout(r, 500));
-  } while (polls <= 10);
+  } while (polls <= 20);
 
-  if (polls >= 10) {
+  if (polls >= 20) {
     throw new Error("Failed to start localstack");
   }
 };
