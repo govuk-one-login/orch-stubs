715f6977e646cdcadcbe600ce942ead80482b15f
Craig Earl <craig.earl@digital.cabinet-office.gov.uk>
2026-07-27 11:31:41 +0100

    BAU: Remove unused dependencies section in provision_all script

--- infrastructure/provision_all.sh
+++ infrastructure/provision_all.sh
@@ -11,16 +11,6 @@ export SKIP_AWS_AUTHENTICATION=true
 export AWS_PAGER=""
 export TAGS_FILE
 
-## Provision dependencies
-for dir in configuration/"$ENVIRONMENT"/*/; do
-  STACK=$(basename "$dir")
-  if [[ $STACK != "$ENVIRONMENT-auth-stub-pipeline" && $STACK != "$ENVIRONMENT-ipv-stub-pipeline" && $STACK != "$ENVIRONMENT-spot-stub-pipeline" && $STACK != "$ENVIRONMENT-ais-stub-pipeline" && -f configuration/$ENVIRONMENT/$STACK/parameters.json ]]; then
-    PARAMETERS_FILE="$(pwd)/configuration/${ENVIRONMENT}/${STACK}/parameters.json"
-    export PARAMETERS_FILE
-    $PROVISION_COMMAND "$ENVIRONMENT" "$STACK" "$STACK" LATEST &
-  fi
-done
-
 ## Provision secure pipelines
 for dir in configuration/"$ENVIRONMENT"/*/; do
   STACK=$(basename "$dir")
