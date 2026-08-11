1c9458bacb2a94bfd911593ccacad6f04c92f8de
Craig Earl <craig.earl@digital.cabinet-office.gov.uk>
2026-08-11 09:56:47 +0100

    BAU: Move ipv-user-identity handler to shared identity folder

--- src/main/shared-identity/handler/user-identity.ts
+++ src/main/shared-identity/handler/user-identity.ts
@@ -8,12 +8,12 @@ import {
   handleErrors,
   methodNotAllowedError,
   createJsonResult,
-} from "../helper/result-helper";
-import { getUserIdentityWithToken } from "./../shared-identity/service/dynamodb-form-response-service.ts";
+} from "../../helper/result-helper.ts";
+import { getUserIdentityWithToken } from "../service/dynamodb-form-response-service.ts";
 import {
   getAccessTokenFromAuthorizationHeader,
   getHeaderValueFromHeaders,
-} from "../util/request-header-helper.ts";
+} from "../../util/request-header-helper.ts";
 
 export const handler: Handler = async (
   event: APIGatewayProxyEvent
